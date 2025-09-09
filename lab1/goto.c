b7f80c2df38b68dfd77cb496df5b12caaeb4cb4f
tuvshin.erdene <tuvshin.erdene25@gmail.com>
2025-09-09 11:39:40 +0800

    finished

--- lab1/goto.c
+++ lab1/goto.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -7,6 +9,16 @@ int main(int argc, char **argv)
         printf("Invalid number of arguments\n");
         return -1;
     }
-    chdir(argv[1]);
+
+    int ret = chdir(argv[1]);
+    if (ret)
+    {
+        fprintf(stderr, "error: %s\n", strerror(errno));
+    }
+    int returnCode = system("list");
+    if (returnCode == 0)
+    {
+        printf("\Run List command successfully\n");
+    }
     return 0;
 }
